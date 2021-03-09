c79b6c2ebb1cf008696a269273ea8a175a3fc7b6
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-03-10 00:22:25 +0700

    adding empty tr to table content so that will give a space

--- PM-328/src/Component1.tsx
+++ PM-328/src/Component1.tsx
@@ -1,6 +1,3 @@
-
-
-
 function Header(props: any) {
   return (
     <div className="top">
@@ -23,30 +20,33 @@ function Header(props: any) {
 }
 
 function GroupHeading(props: any) {
-  return (<tbody>
-    <tr>
-      <td colSpan="5">
-        <b className="table-name">{props.title}</b>
-      </td>
-    </tr>
-  </tbody>);
+  return (
+    <tbody>
+      <tr>
+        <td colSpan="5">
+          <b className="table-name">{props.title}</b>
+        </td>
+      </tr>
+    </tbody>
+  );
 }
 
 function TableHeading(props: any) {
-  return (<tbody>
-    <tr>
-      <th>Done</th>
-      <th className="table-header">Name</th>
-      <th className="table-header">Task</th>
-      <th className="table-header">Status</th>
-      <th className="table-header">Deadline</th>
-      <th className="table-header">Assigned to</th>
-    </tr>
-  </tbody>);
+  return (
+    <tbody>
+      <tr>
+        <th>Done</th>
+        <th className="table-header">Name</th>
+        <th className="table-header">Task</th>
+        <th className="table-header">Status</th>
+        <th className="table-header">Deadline</th>
+        <th className="table-header">Assigned to</th>
+      </tr>
+    </tbody>
+  );
 }
 
 function TableItems(props: any) {
-
   const handleClick = (key) => {
     props.switchButton(key);
   };
@@ -58,7 +58,9 @@ function TableItems(props: any) {
           <td width="5%" className="table-box">
             <button
               onClick={() => handleClick(item.key)}
-              className={`${props.buttons[item.key] ? "box-green" : "box-gray"}`}
+              className={`${
+                props.buttons[item.key] ? "box-green" : "box-gray"
+              }`}
             ></button>
           </td>
           <td width="40%" className="table-item">
@@ -78,6 +80,9 @@ function TableItems(props: any) {
           </td>
         </tr>
       ))}
+      <tr className="spacing-after">
+        <td colSpan="6"></td>
+      </tr>
     </tbody>
   );
 }
@@ -87,11 +92,11 @@ function Component1(props: any) {
 
   const [buttons, setButtons] = React.useState({});
 
-  let switchButton = (key:string) => {
-    let newButtons = { ...buttons};
+  let switchButton = (key: string) => {
+    let newButtons = { ...buttons };
     newButtons[key] = !newButtons[key];
     setButtons(newButtons);
-  }
+  };
 
   React.useEffect(() => {
     BridgeStyling.loadStyle("./src/Component1.scss").then((css) => {
@@ -119,12 +124,17 @@ function Component1(props: any) {
             </p>
             <div className="table-top">
               <table className="table">
-              {_data.map( d => (<React.Fragment key={d.title}>
-                <GroupHeading title={d.title} />
-                <TableHeading />
-                <TableItems buttons={buttons} group={d} switchButton={switchButton} />
-                </React.Fragment>)
-              )}
+                {_data.map((d) => (
+                  <React.Fragment key={d.title}>
+                    <GroupHeading title={d.title} />
+                    <TableHeading />
+                    <TableItems
+                      buttons={buttons}
+                      group={d}
+                      switchButton={switchButton}
+                    />
+                  </React.Fragment>
+                ))}
               </table>
             </div>
           </div>
